6352931c008c9d5df26de1a4ae063792055ec8fc
MohamedOsman <Mohamed.Osman.dev@gmail.com>
2017-11-26 16:17:09 +0100

    Added entity Book and presistence.xml file

---
+++ src/main/java/com/osmanprojects/bookstore/model/Language.java
@@ -0,0 +1,9 @@
+package com.osmanprojects.bookstore.model;
+
+/**
+ * Created by MohamedOsman on 2017-11-26.
+ */
+public enum Language {
+
+    ENGLISH, FRENCH, SPANISH, PROTUGUESE, ITALIAN, FINISH, GERMAN, DEUTSCH, RUSSIAN
+}
